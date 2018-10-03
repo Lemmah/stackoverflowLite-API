f027ceef78fef7d5033efc0fd3d68db87b38a949
Lemmah <lemayiannakolah@gmail.com>
2018-10-03 19:43:46 +0300

    feat(questionsModel): sort answers on votes and updatedAt
    - add a pre-save hook to sort answers using a compare function sortAnswers.
    - the compare function sorts by votes and if votes are equal,
    - sorts by most recently updated first.
    
    [Delivers #sortAnswers]

--- models/questions.js
+++ models/questions.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const sortAnswers = (ans1, ans2) => {
+  // - ans1 before ans2
+  // 0 no change
+  // + ans2 before ans1
+  if (ans1.votes == ans2.votes) {
+    return ans1.updatedAt - ans2.updatedAt;
+  }
+  return ans1.votes - ans2.votes;
+}
+
 const AnswerSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
@@ -11,10 +21,30 @@ const AnswerSchema = new Schema({
   votes: {type: Number, default: 0}
 });
 
+AnswerSchema.method('update', (updates, callback) => {
+  Object.assign(this, updates, {updatedAt: new Date()});
+  this.parent().save(callback);
+});
+
+AnswerSchema.method('vote', (vote, callback) => {
+  if (vote == 'up') {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent.save(callback);
+});
+
 const QuestionSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
   answers: [AnswerSchema]
+});
+
+// Presave callback for question schema
+QuestionSchema.pre('save', (next) => {
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
