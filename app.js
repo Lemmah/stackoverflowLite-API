458d4d98b3a126618856c4cb3b42c6a1901acd85
Lemmah <lemayiannakolah@gmail.com>
2018-10-03 19:24:42 +0300

    feat(app): use mongodb for data
    - configure mongodb for use in the application.
    - create question schemas and answers schemas.
    - create a questions model and export it.
    
    [Delivers #dataHandling]

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ const routes = require('./routes/');
 app.use(logger('dev'));
 app.use(jsonParser());
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/sandbox', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('Connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('db connection successful');
+});
+
 app.use('/questions', routes);
 
 // catch 404 and forrward to error handler
