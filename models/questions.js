ba70a9cd0332953150e45dab04cac94968c54b4e
Lemmah <lemayiannakolah@gmail.com>
2018-10-04 19:23:51 +0300

    feat(routes|models): connect the API to db
    - configure the questions schema.
    - modify routes to interact with the schema.
    
    [Delivers #useDB]

--- models/questions.js
+++ models/questions.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const sortAnswers = (ans1, ans2) => {
+function compareAnswers(ans1, ans2) {
   // - ans1 before ans2
   // 0 no change
   // + ans2 before ans1
+  console.log(ans1.updatedAt, ans2.updatedAt);
   if (ans1.votes == ans2.votes) {
-    return ans1.updatedAt - ans2.updatedAt;
+    return ans2.updatedAt - ans1.updatedAt;
   }
-  return ans1.votes - ans2.votes;
+  return ans2.votes - ans1.votes;
 }
 
 const AnswerSchema = new Schema({
@@ -21,18 +22,18 @@ const AnswerSchema = new Schema({
   votes: {type: Number, default: 0}
 });
 
-AnswerSchema.method('update', (updates, callback) => {
+AnswerSchema.method('update', function(updates, callback) {
   Object.assign(this, updates, {updatedAt: new Date()});
   this.parent().save(callback);
 });
 
-AnswerSchema.method('vote', (vote, callback) => {
+AnswerSchema.method('vote', function(vote, callback) {
   if (vote == 'up') {
     this.votes += 1;
   } else {
     this.votes -= 1;
   }
-  this.parent.save(callback);
+  this.parent().save(callback);
 });
 
 const QuestionSchema = new Schema({
@@ -41,9 +42,9 @@ const QuestionSchema = new Schema({
   answers: [AnswerSchema]
 });
 
-// Presave callback for question schema
-QuestionSchema.pre('save', (next) => {
-  this.answers.sort(sortAnswers);
+// Presave callback to sort before saving
+QuestionSchema.pre('save', function(next) {
+  this.answers.sort(compareAnswers);
   next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,60 +2,90 @@
 
 const express = require('express');
 const router = express.Router();
+const Question = require('./../models/questions').Question;
 
-// GET /questions
-router.get('/', (req, res) => {
-  return res.json({response: 'You sent me a GET request'});
-});
-
-// POST /questions
-router.post('/', (req, res) => {
-  return res.json({
-    response: 'You sent me a POST request',
-    body: req.body
+// handler for qID param
+router.param('qID', (req, res, next, id) => {
+  Question.findById(id, (err, questionDoc) => {
+    if (err) return next(err);
+    if (!questionDoc) {
+      err = new Error('Not found');
+      err.status = 404;
+      return next(err);
+    }
+    req.question = questionDoc;
+    next();
   });
 });
 
+// handler for aID param
+router.param('aID', (req, res, next, id) => {
+  req.answer = req.question.answers.id(id);
+  if (!req.answer) {
+    const err = new Error('Not found');
+    err.status = 404;
+    return next(err);
+  }
+  next();
+});
+
 // GET /questions
-router.get('/:id', (req, res) => {
-  return res.json({response: 'You sent me a GET request at ID ' + req.params.id});
+router.get('/', (req, res, next) => {
+  Question.find({})
+    .sort({createdAt: -1})
+    .exec((err, questions) => {
+      if (err) return next(err);
+      res.json(questions);
+    });
+});
+
+// POST /questions
+router.post('/', (req, res, next) => {
+  const question = new Question(req.body);
+  question.save((err, question) => {
+    if (err) return next(err);
+    res.status(201);
+    res.json(question);
+  });
+});
+
+// GET /questions/id
+router.get('/:qID', (req, res, next) => {
+  res.json(req.question);
 });
 
 // GET /questions/:id/answers
 router.get('/:qID/answers', (req, res) => {
-  return res.json({
-    response: 'You sent me a PUT request to /answers',
-    questionId: req.params.qID,
-    answerId: req.params.aID
-  });
+  res.json(req.question.answers);
 });
 
 // POST /questions/:id/answers
-router.post('/:qID/answers', (req, res) => {
-  return res.json({
-    response: 'You sent me a POST request to /answers',
-    questionId: req.params.qID,
-    body: req.body
+router.post('/:qID/answers', (req, res, next) => {
+  req.question.answers.push(req.body);
+  req.question.save((err, question) => {
+    if (err) return next(err);
+    res.status(201);
+    res.json(question);
   });
 });
 
 // PUT /questions/:id/answers/:id
 router.put('/:qID/answers/:aID', (req, res) => {
-  return res.json({
-    response: 'You sent me a PUT request to /answers',
-    questionId: req.params.qID,
-    answerId: req.params.aID,
-    body: req.body
+  req.answer.update(req.body, (err, results) => {
+    if (err) return next(err);
+    res.json(results);
   });
 });
 
 // DELETE /questions/:id/answers/:id
 router.delete('/:qID/answers/:aID', (req, res) => {
-  return res.json({
-    response: 'You sent me a DELETE request to /answers',
-    questionId: req.params.qID,
-    answerId: req.params.aID
-  });
+  req.answer.remove((err) => {
+    if (err) return next(err);
+    req.question.save((err, question) => {
+      if (err) return next(err);
+      res.json(question);
+    })
+  })
 });
 
 // POST /questions/:id/answers/:id/vote-up
@@ -67,14 +97,13 @@ router.post('/:qID/answers/:aID/vote-:dir', (req, res, next) => {
     err.status = 404;
     next(err);
   } else {
+    req.vote = req.params.dir;
     next();
   }
-}, (req, res) => {
-  return res.json({
-    response: 'You sent me a POST request to /vote-' + req.params.dir,
-    questionId: req.params.qID,
-    answerId: req.params.aID,
-    vote: req.params.dir
+}, (req, res, next) => {
+  req.answer.vote(req.vote, (err, question) => {
+    if (err) return next(err);
+    res.json(question);
   });
 });
 
